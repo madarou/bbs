f920becc0c9a77560eabf50694ea59ca54ac3cbf
madarou <2016333261@qq.com>
2016-10-29 21:08:29 +0800

    comment out couldn't access sites

--- bbs-crawler/src/main/java/com/makao/bbs_crawler/quartz/ItemCrawlerJob.java
+++ bbs-crawler/src/main/java/com/makao/bbs_crawler/quartz/ItemCrawlerJob.java
@@ -62,14 +62,14 @@ public class ItemCrawlerJob
     int threadcount = Configure.configure.getThreadcount();
     
     ExecutorService executor = Executors.newFixedThreadPool(threadcount);
-//    executor.execute(new ItemCrawler_FDU(itemqueue_fdu, "com/makao/bbs_crawler/xml/BBSUrls_FDU.xml"));
-//    executor.execute(new ItemCrawler_SJ(itemqueue_sj, "com/makao/bbs_crawler/xml/BBSUrls_SJ.xml"));
-//    executor.execute(new ItemCrawler_PKU(itemqueue_pku, "com/makao/bbs_crawler/xml/BBSUrls_PKU.xml"));
-//    executor.execute(new ItemCrawler_QH(itemqueue_qinghua, "com/makao/bbs_crawler/xml/BBSUrls_QH.xml"));
+    executor.execute(new ItemCrawler_FDU(itemqueue_fdu, "com/makao/bbs_crawler/xml/BBSUrls_FDU.xml"));
+    executor.execute(new ItemCrawler_SJ(itemqueue_sj, "com/makao/bbs_crawler/xml/BBSUrls_SJ.xml"));
+    executor.execute(new ItemCrawler_PKU(itemqueue_pku, "com/makao/bbs_crawler/xml/BBSUrls_PKU.xml"));
+    executor.execute(new ItemCrawler_QH(itemqueue_qinghua, "com/makao/bbs_crawler/xml/BBSUrls_QH.xml"));
     executor.execute(new ItemCrawler_NJU(itemqueue_nju, "com/makao/bbs_crawler/xml/BBSUrls_NJU.xml"));
-//    executor.execute(new ItemCrawler_BYR(itemqueue_byr, "com/makao/bbs_crawler/xml/BBSUrls_BYR.xml"));
-//    executor.execute(new ItemCrawler_ECNU(itemqueue_ecnu, "com/makao/bbs_crawler/xml/BBSUrls_ECNU.xml"));
-//    executor.execute(new ItemCrawler_ECUST(itemqueue_ecust, "com/makao/bbs_crawler/xml/BBSUrls_ECUST.xml"));
+//    executor.execute(new ItemCrawler_BYR(itemqueue_byr, "com/makao/bbs_crawler/xml/BBSUrls_BYR.xml"));//没有账号密码
+//    executor.execute(new ItemCrawler_ECNU(itemqueue_ecnu, "com/makao/bbs_crawler/xml/BBSUrls_ECNU.xml"));//网络差，connection time out
+//    executor.execute(new ItemCrawler_ECUST(itemqueue_ecust, "com/makao/bbs_crawler/xml/BBSUrls_ECUST.xml"));//原网址停用了，新网址http://bbs.myecust.com/需要账号密码
     
     executor.shutdown();
     try {
@@ -94,10 +94,10 @@ public class ItemCrawlerJob
     executor.execute(new ArticleCrawler_PKU(itemqueue_pku, Configure.configure.getSleeptime()));
     executor.execute(new AritcleCrawler_QH(itemqueue_qinghua, Configure.configure.getSleeptime()));
     executor.execute(new ArticleCrawler_NJU(itemqueue_nju, Configure.configure.getSleeptime()));
-    executor.execute(new ArticleCrawler_BYR(itemqueue_byr, Configure.configure.getSleeptime()));
-    executor.execute(new ArticleCrawler_ECNU(itemqueue_ecnu, Configure.configure.getSleeptime()));
-    executor.execute(new ArticleCrawler_ECUST(itemqueue_ecust, Configure.configure.getSleeptime()));
-    executor.execute(new ArticleCrawler_BJTU(itemqueue_bjtu, Configure.configure.getSleeptime()));
+//    executor.execute(new ArticleCrawler_BYR(itemqueue_byr, Configure.configure.getSleeptime()));
+//    executor.execute(new ArticleCrawler_ECNU(itemqueue_ecnu, Configure.configure.getSleeptime()));
+//    executor.execute(new ArticleCrawler_ECUST(itemqueue_ecust, Configure.configure.getSleeptime()));
+//    executor.execute(new ArticleCrawler_BJTU(itemqueue_bjtu, Configure.configure.getSleeptime()));
     executor.shutdown();
     try
     {
